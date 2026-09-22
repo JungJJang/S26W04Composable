0d60516462cd7f32f6b31b500148aac39e62b427
JungJJang <102155344+JungJJang@users.noreply.github.com>
2026-09-22 14:39:56 +0900

    🎉Project 시작

--- app/src/main/java/kr/ac/kumoh/s20220633/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20220633/s26w04composable/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -19,29 +20,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             S26W04ComposableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S26W04ComposableTheme {
-        Greeting("Android")
+        }
     }
 }
